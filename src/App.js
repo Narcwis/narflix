b962085ba1ba78596cf325b0d0a3fcfb721ce68c
Narcis <narcis.cristea@gmail.com>
2019-11-20 00:02:58 +0000

    finished functionality

--- src/App.js
+++ src/App.js
@@ -5,24 +5,48 @@ import axios from "axios";
 import Button from "./components/Button";
 import Description from "./components/Description";
 import FilmCard from "./components/FilmCard";
+import FilmGrid from "./components/FilmGrid";
+import _ from "lodash";
 
 function App() {
     const [films, setFilms] = useState([]);
+    const [count, setCount] = useState(5);
+    const [page, setPage] = useState(1);
 
-    useEffect(() => {});
-    // const getData = url => {
-    //     axios
-    //         .get(url)
-    //         .then(({ data }) => console.log(data))
-    //         .catch(error => console.log(error));
-    // };
-    // getData("https://api.themoviedb.org/3/discover/movie?api_key=e94c3f3fab193264594666c3cf7639b1&sort_by=popularity.desc");
+    const getFilms = page => {
+        const url = `https://api.themoviedb.org/3/discover/movie?api_key=e94c3f3fab193264594666c3cf7639b1&sort_by=popularity.desc&page=${page}`;
+        axios
+            .get(url)
+            .then(({ data }) => {
+                setFilms([...films, ...data.results]);
+            })
+            .catch(error => console.log(error));
+    };
+
+    useEffect(() => {
+        getFilms(page);
+    }, []);
+
+    const moreFilms = () => {
+        if (count === films.length) {
+            getFilms(page + 1);
+            setPage(page + 1);
+        }
+        setCount(count + 5);
+    };
+
     return (
         <div className="App">
             <Logo />
             <Description>For only 25 schmeckles per month you can get all these films!</Description>
-            {films.length > 0 ? films.map(film => <FilmCard film={film}></FilmCard>) : <Description>No films yet sorry :(</Description>}
-            <Button>Moar plz</Button>
+            <FilmGrid>
+                {films.length > 0 ? (
+                    _.take(films, count).map(film => <FilmCard film={film}></FilmCard>)
+                ) : (
+                    <Description>No films yet sorry :(</Description>
+                )}
+            </FilmGrid>
+            <Button onClick={moreFilms}>Moar plz</Button>
         </div>
     );
 }
---
+++ src/components/FilmGrid.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+const FilmGrid = styled.div`
+    text-align: center;
+`;
+
+export default FilmGrid;
